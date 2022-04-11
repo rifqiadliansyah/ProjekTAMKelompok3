f4ee00851af61976f0fe36127cee25b87659eb64
rifqiadliansyah <Rifqi Adliansyah>
2022-04-11 22:12:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projekkelompok3/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.projekkelompok3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            Intent i = new Intent(MainActivity.this, HomePage.class);
+            startActivity(i);
+            finish();
+            }
+        },1500);
+
+    }
+}
