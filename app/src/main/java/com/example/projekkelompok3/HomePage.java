410c6ea4a70057fd08d0efadf11fa26f24fc2ee0
rifqiadliansyah <Rifqi Adliansyah>
2022-04-13 09:50:09 +0700

    Buat navigations bottom

--- app/src/main/java/com/example/projekkelompok3/HomePage.java
+++ app/src/main/java/com/example/projekkelompok3/HomePage.java
@@ -1,14 +1,47 @@
 package com.example.projekkelompok3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomePage extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        getSupportActionBar().hide();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                                homeFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                                profileFragment).commit();
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
+
+
+
     }
 }
