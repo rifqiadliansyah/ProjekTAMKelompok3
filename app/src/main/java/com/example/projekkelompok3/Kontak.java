9b5da9faaed1261cb751b32b2e85d301e6dc611e
RayhanEVM <rayhan26akbar@gmail.com>
2022-04-22 23:22:35 +0700

    3rdBranchUpdate
    Update CardView Prototype

---
+++ app/src/main/java/com/example/projekkelompok3/Kontak.java
@@ -0,0 +1,23 @@
+package com.example.projekkelompok3;
+
+public class Kontak {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    private String foto;
+}
