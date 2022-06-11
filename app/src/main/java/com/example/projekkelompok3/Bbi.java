e10ef0e3200ace41b35987bf7db970e045a0bf03
RayhanEVM <rayhan26akbar@gmail.com>
2022-06-11 23:31:29 +0700

    Data Awal
    Mencoba memasukan DataStore

--- app/src/main/java/com/example/projekkelompok3/Bbi.java
+++ app/src/main/java/com/example/projekkelompok3/Bbi.java
@@ -1,8 +1,11 @@
 package com.example.projekkelompok3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.datastore.preferences.core.Preferences;
+import androidx.datastore.preferences.core.PreferencesKeys;
+import androidx.datastore.preferences.rxjava3.RxPreferenceDataStoreBuilder;
+import androidx.datastore.rxjava3.RxDataStore;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,12 +14,16 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import io.reactivex.rxjava3.core.Flowable;
+
 public class Bbi extends AppCompatActivity {
 
     EditText tinggiBadan;
     TextView bbi;
+    private TextView Bbilama;
     private RadioButton pria, wanita;
-
+    private RxDataStore<Preferences> dataStore;
+    private Preferences.Key<String> KEY_NAME = PreferencesKeys.stringKey("name");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,19 @@ public class Bbi extends AppCompatActivity {
         wanita=findViewById(R.id.wanita);
         tinggiBadan=findViewById(R.id.tinggibadan);
         bbi=findViewById(R.id.bbi);
+        Bbilama = findViewById(R.id.bbilama);
         Button proses = findViewById(R.id.button1);
         proses.setOnClickListener(this::onClick);
+
+        dataStore = new RxPreferenceDataStoreBuilder(getApplicationContext(), "profile").build();
+        Flowable<String> flowable = dataStore.data().map(preferences ->{
+            String name = "-";
+            if (preferences.get(KEY_NAME)!=null){
+                name = preferences.get(KEY_NAME);
+            }
+            Bbilama.setText(name);
+            return name;
+        });
 
     }
     private void onClick(View v) {
@@ -53,9 +71,5 @@ public class Bbi extends AppCompatActivity {
     }
 
     }
-
-
-
-
 
 }
